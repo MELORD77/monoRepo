e97120c900a5b0943b1810a7cdbf8c4081fd0039
MELORD <qahramonmansurov98@mail.ru>
2024-10-19 09:25:17 +0500

    first commit

---
+++ home/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+// src/pages/HomePage.tsx
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Home Page</h1>
+      <p className="mt-4">Welcome to the home page!</p>
+    </div>
+  );
+};
+
+export default HomePage;
